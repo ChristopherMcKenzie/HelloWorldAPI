98090e3ac7d8cdfa7a8ed45fbe879fc09664c275
ChristopherMcKenzie <christophermckenzie.1997@gmail.com>
2021-11-02 13:33:07 +0000

    First commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public async Task<String> Get(string name)
+        {
+            Request.Headers.TryGetValue("User-Agent", out var header);          
+            await Task.Delay(100);
+            return name + " " + header;
+        }
+    }
+}
---
+++ Middleware/MiddleWare.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAPI.Middleware
+{
+    public class MiddleWare
+    {
+        private readonly RequestDelegate _next;
+
+        public MiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.Headers.TryGetValue("User-Agent", out var header);
+
+            await _next(context);
+            
+            Console.WriteLine(header);
+        }
+    }
+    
+    
+}
